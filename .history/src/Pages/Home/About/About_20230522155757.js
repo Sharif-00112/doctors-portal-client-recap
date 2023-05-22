13ae4cac711e90420c9ce404417fd95f632e29b4
BGI-HO-IT-Exe-Sharif <it22@bengal.com.bd>
2023-05-22 15:59:01 +0600

    creating pages

--- .history/src/Pages/Home/About/About_20230522155757.js
+++ .history/src/Pages/Home/About/About_20230522155757.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const About = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <h4>About Test</h4>
         </div>
     );
 };
 
-export default Home;
+export default About;
